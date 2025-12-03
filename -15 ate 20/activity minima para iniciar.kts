c574f4da655243733905546e4e0a740cba86907f
jamili <jamiligabriela29@gmail.com>
2025-12-02 23:55:23 -0300

    Projeto completo 1

--- -15 ate 20/activity minima para iniciar.kts
+++
@@ -1,7 +0,0 @@
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        // MotionLayout já tem OnClick toggle definido no scene — nada extra necessário
-    }
-}
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 android {
@@ -29,6 +31,15 @@ android {
                 "proguard-rules.pro"
             )
         }
+            debug {
+                buildConfigField("String", "SUPABASE_URL", "\"https://vwipzzvyziqwtfwivhns.supabase.co\"")
+                buildConfigField("String", "SUPABASE_KEY", "\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3aXB6enZ5emlxd3Rmd2l2aG5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1OTc1MDEsImV4cCI6MjA3NzE3MzUwMX0.g8aeCqgF3_UjKrL6EEMdU_AfS3i8UZF6Cve1vicGnkU\"")
+            }
+            release {
+                buildConfigField("String", "SUPABASE_URL", "\"https://vwipzzvyziqwtfwivhns.supabase.co\"")
+                buildConfigField("String", "SUPABASE_KEY", "\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3aXB6enZ5emlxd3Rmd2l2aG5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1OTc1MDEsImV4cCI6MjA3NzE3MzUwMX0.g8aeCqgF3_UjKrL6EEMdU_AfS3i8UZF6Cve1vicGnkU\"")
+
+            }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -58,9 +69,20 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation("com.github.bumptech.glide:glide:4.16.0")   // << ADICIONE ESTA LINHA
-    kapt("com.github.bumptech.glide:compiler:4.16.0")        // << ADICIONE ESTA LINHA
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    kapt("com.github.bumptech.glide:compiler:4.16.0")
 
+    // Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-compiler:2.51.1")
+
+    // Google Sign-In
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
@@ -70,4 +92,10 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
+    implementation(platform("io.github.jan-tennert.supabase:bom:3.2.6"))
+    implementation("io.github.jan-tennert.supabase:postgrest-kt")
+    implementation("io.github.jan-tennert.supabase:auth-kt")
+    implementation("io.github.jan-tennert.supabase:realtime-kt")
+    implementation("io.github.jan-tennert.supabase:storage-kt")
+    implementation("io.ktor:ktor-client-android:3.3.3")
 }
